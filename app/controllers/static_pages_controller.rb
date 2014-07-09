439f789278c2f9cb5d8dce12d86495b489eba22a
Juan Ortiz <juandefelix@yahoo.es>
2014-07-09 13:47:02 -0500

    removed requires and removed unnecessary method in the controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,18 +1,4 @@
-require 'open-uri'
-require 'pry'
-
 class StaticPagesController < ApplicationController
   def index
-  end
-
-  def create
-    request_address = "http://maps.google.com/maps/api/geocode/json?address=2044+W+Homer+St+Chicagi,+IL&sensor=false"
-    uri = URI.parse(request_address)
-    @json = uri.read
-    # binding.pry
-    @response = params['q']
-    render :response
-
-    repond
   end
 end
